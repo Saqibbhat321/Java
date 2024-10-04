41b3bbfabd318ece7cd7bada4ffeee25ec762af5
Saqibbhat321 <saqib.bhat.x@gmail.com>
2024-10-04 21:03:40 +0530

    Encapsulation

---
+++ encapsulation/src/com/xworkz/cap/external/User.java
@@ -0,0 +1,13 @@
+package com.xworkz.cap.external;
+
+import com.xworkz.cap.internal.Television;
+
+public class User {
+
+
+     private Television television;
+    public User(Television television)
+    {
+        this.television = television;
+    }
+}
