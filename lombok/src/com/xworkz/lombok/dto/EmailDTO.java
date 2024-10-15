f40e06ec3609802d6604bacfd1cca4d28c0d8778
Saqibbhat321 <saqib.bhat.x@gmail.com>
2024-10-15 22:05:16 +0530

    lombok

---
+++ lombok/src/com/xworkz/lombok/dto/EmailDTO.java
@@ -0,0 +1,14 @@
+package com.xworkz.lombok.dto;
+import java.io.Serializable;
+
+public class EmailDTO{
+
+
+    private String from;
+    private String to;
+    private String subject;
+    private String message;
+
+
+
+}
---
+++ lombok/src/com/xworkz/lombok/runner/LombokRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.lombok.runner;
+
+import com.xworkz.lombok.dto.EmailDTO;
+
+public class LombokRunner {
+
+    public static void main(String[] args) {
+
+
+        EmailDTO emailDTO = new EmailDTO();
+    }
+}
